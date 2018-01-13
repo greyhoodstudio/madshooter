a88040c09d58bf41fc3989f92c74b08aa4f0f446
DOOP\Sungbin <dsbzdm@gmail.com>
2018-01-13 22:44:08 +0900

    First Day :(

---
+++ Assets/Scripts/LobbyGrid.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(MeshFilter), typeof(MeshRenderer))]
+public class LobbyGrid : MonoBehaviour {
+
+    Mesh mesh;
+    Vector3[] vertices;
+    int[] triangles;
+
+    //grid settings
+    public float cellSize = 1;
+    public Vector3 gridOffset;
+    public int gridSize;
+
+    //initialization
+    private void Awake()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+    }
+
+    private void Start()
+    {
+        MakeLobbyGrid();
+        UpdateMesh();
+    }
+
+    void MakeLobbyGrid()
+    {
+        //set array sizes
+        vertices = new Vector3[gridSize * gridSize * 4];
+        triangles = new int[gridSize * gridSize * 6];
+
+        //set tracker integers
+        int v = 0;
+        int t = 0;
+
+        //set vertex offset
+        float vertexOffset = cellSize * 0.5f;
+
+        //generate cells
+        for (int x = 0; x < gridSize; x++)
+        {
+            for (int y = 0; y < gridSize; y++)
+            {
+                //set cell offset
+                Vector3 cellOffset = new Vector3(x * cellSize, y * cellSize);
+
+                //populate vertex array
+                vertices[v] = new Vector3 (-vertexOffset, -vertexOffset, 0) + cellOffset + gridOffset;
+                vertices[v+1] = new Vector3 (-vertexOffset, vertexOffset, 0) + cellOffset + gridOffset;
+                vertices[v+2] = new Vector3 (vertexOffset, -vertexOffset, 0) + cellOffset + gridOffset;
+                vertices[v+3] = new Vector3 (vertexOffset, vertexOffset, 0) + cellOffset + gridOffset;
+
+                //populate triangle array
+                triangles[t] = v;
+                triangles[t + 1] = triangles[t + 4] = v + 1;
+                triangles[t + 2] = triangles[t + 3] = v + 2;
+                triangles[t + 5] = v + 3;
+
+                //update tracker integers
+                v += 4;
+                t += 6;
+            }
+        }
+    }
+
+    void UpdateMesh()
+    {
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+
+    }
+}
---
+++ Assets/Scripts/NetworkSocket.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class NetworkSocket : MonoBehaviour
+{
+
+    // Variables
+
+    // Set host, post: EDIT IN ENGINE
+    public String host = "localhost";
+    public Int32 port = 50000;
+
+    internal Boolean socket_ready = false;
+    internal String input_buffer = "";
+    TcpClient tcp_socket;
+    NetworkStream net_stream;
+
+    StreamWriter socket_writer;
+    StreamReader socket_reader;
+	
+	// Update is called once per frame
+	void Update () {
+
+        string received_data = readSocket();
+
+        if (received_data != "")
+        {
+            Debug.Log(received_data);
+        }
+
+    }
+
+    void Awake()
+    {
+        setupSocket();
+    }
+
+    void OnApplicationQuit()
+    {
+        closeSocket();
+    }
+
+    public void setupSocket()
+    {
+        try
+        {
+            tcp_socket = new TcpClient(host, port);
+            net_stream = tcp_socket.GetStream();
+            socket_writer = new StreamWriter(net_stream);
+            socket_reader = new StreamReader(net_stream);
+            socket_ready = true;
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Socket error: " + e);
+        }
+    }
+
+    public void closeSocket()
+    {
+        if (!socket_ready)
+            return;
+
+        socket_writer.Close();
+        socket_reader.Close();
+        tcp_socket.Close();
+        socket_ready = false;
+    }
+
+    public String readSocket()
+    {
+        if (!socket_ready)
+            return "";
+
+        if (net_stream.DataAvailable)
+            return socket_reader.ReadLine();
+
+        return "";
+    }
+
+    public void writeSocket(string line)
+    {
+        if (!socket_ready)
+            return;
+
+        line = line + "\r\n";
+        socket_writer.Write(line);
+        socket_writer.Flush();
+    }
+}
